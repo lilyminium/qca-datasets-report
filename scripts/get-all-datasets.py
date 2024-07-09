c275d4809a49dc67e6acc0275a69f44c15af027d
Lily Wang <lily@mdanalysis.org>
2024-07-09 14:45:11 +1000

    start trying to download

---
+++ scripts/get-all-datasets.py
@@ -0,0 +1,29 @@
+import click
+import tqdm
+
+from qcportal import PortalClient
+import pandas as pd
+
+
+@click.command()
+@click.option(
+    "--output-file",
+    type=click.Path(exists=False, dir_okay=False, file_okay=True)
+)
+def main(
+    output_file: str,
+):
+    client = PortalClient("https://api.qcarchive.molssi.org:443")
+    datasets = client.list_datasets()
+    # get specifications
+    for entry in tqdm.tqdm(datasets):
+        specifications = client.get_dataset(entry["dataset_type"], entry["dataset_name"])
+        entry["specifications"] = sorted(specifications)
+
+    df = pd.DataFrame(datasets)
+    df.to_csv(output_file, index=False)
+    print(f"Saved {len(df)} datasets to {output_file}")
+
+
+if __name__ == "__main__":
+    main()
